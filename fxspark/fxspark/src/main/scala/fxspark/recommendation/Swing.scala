f79c50aef1f886a36a883227c625ee491e6ec2fa
nifannn <nifan@nifans-MacBook-Air.local>
2020-02-01 18:34:54 +0800

    update swing

--- fxspark/fxspark/src/main/scala/fxspark/recommendation/Swing.scala
+++ fxspark/fxspark/src/main/scala/fxspark/recommendation/Swing.scala
@@ -4,7 +4,6 @@ import org.slf4j.{Logger, LoggerFactory}
 import scala.io.Source
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import scopt.OParser
-import fxspark.recommendation.SwingModel
 
 object Swing {
   val logger = LoggerFactory.getLogger(getClass.getName)
@@ -13,9 +12,20 @@ object Swing {
                      hqlFile: String = "",
                      taskMode: String = "",
                      saveMode: String = "",
+                     maxUser: Int = 800,
                      topn: Int = 100,
                      out: String = "",
-                     saveScore: String = ""
+                     saveScore: String = "",
+                     overwrite: Boolean = false,
+                     itemICol: String = "item_i",
+                     itemJCol: String = "item_j",
+                     scoreCol: String = "score",
+                     itemCol: String = "item",
+                     itemListCol: String = "item_list",
+                     partitionCol: String = "",
+                     partitionVal: String = "",
+                     fieldDelimiter: String = "\t",
+                     collectionDelimiter: String = ","
                    )
   val builder = OParser.builder[Config]
   val parser = {
@@ -28,7 +38,7 @@ object Swing {
         .valueName("<hql_file>")
         .action((x, c) => c.copy(hqlFile = x))
         .text("input hql file"),
-      opt[String]('s', "save-mode")
+      opt[String]('m', "save-mode")
         .required()
         .action((x, c) => c.copy(saveMode = x))
         .text("save mode, hive | text"),
@@ -36,6 +46,39 @@ object Swing {
         .required()
         .action((x, c) => c.copy(out = x))
         .text("output"),
+      opt[Boolean]('r', "overwrite")
+        .action((x, c) => c.copy(overwrite = x))
+        .text("whether overwrite"),
+      opt[Int]('u', "max-user")
+        .action((x, c) => c.copy(maxUser = x))
+        .text("max user number"),
+      opt[String]("item-i")
+        .action((x, c) => c.copy(itemICol = x))
+        .text("item i column of item similarity hive table"),
+      opt[String]("item-j")
+        .action((x, c) => c.copy(itemJCol = x))
+        .text("item j column of item similarity hive table"),
+      opt[String]("score-col")
+        .action((x, c) => c.copy(scoreCol = x))
+        .text("item similarity score column of item similarity hive table"),
+      opt[String]("item-col")
+        .action((x, c) => c.copy(itemCol = x))
+        .text("item column of topn result hive table"),
+      opt[String]("itemlist-col")
+        .action((x, c) => c.copy(itemListCol = x))
+        .text("recommended item list column of topn result hive table"),
+      opt[String]("partition-col")
+        .action((x, c) => c.copy(partitionCol = x))
+        .text("partition column name"),
+      opt[String]("partition-val")
+        .action((x, c) => c.copy(partitionVal = x))
+        .text("partition column value"),
+      opt[String]("field-delimiter")
+        .action((x, c) => c.copy(fieldDelimiter = x))
+        .text(raw"field delimiter, only used when saving in text format, default \t"),
+      opt[String]("collection-delimiter")
+        .action((x, c) => c.copy(collectionDelimiter = x))
+        .text("collection delimiter, only used when saving in text format, default ,"),
       cmd("score")
         .action((_, c) => c.copy(taskMode = "score"))
         .text("compute item similarity only"),
@@ -82,12 +125,41 @@ object Swing {
 
     config.taskMode match {
       case "score" => config.saveMode match {
-        case "hive" => SwingModel.computeItemSimilarity(df).saveItemSimilarityAsHiveTable(config.out)
-        case "text" => SwingModel.computeItemSimilarity(df).saveItemSimilarityAsTextFile(config.out)
+        case "hive" => SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity().saveItemSimilarityAsHiveTable(spark, config.out,
+                        config.itemICol, config.itemJCol, config.scoreCol, config.overwrite,
+                        config.partitionCol, config.partitionVal)
+        case "text" => SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity().saveItemSimilarityAsTextFile(spark, config.out,
+          config.overwrite, config.fieldDelimiter, config.collectionDelimiter)
       }
       case "topn" => config.saveMode match {
         case "hive" => {
-          if config.saveScore.length > 0 SwingModel.computeItemSimilarity
+          if (config.saveScore.length>0) {
+            SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity()
+              .saveItemSimilarityAsHiveTable(spark, config.saveScore, config.itemICol, config.itemJCol, config.scoreCol,
+                config.overwrite, config.partitionCol, config.partitionVal)
+              .computeTopn(config.topn)
+              .saveTopnAsHiveTable(spark, config.out, config.itemCol, config.itemListCol, config.overwrite,
+                config.partitionCol, config.partitionVal)
+          }
+          else {
+            SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity()
+              .computeTopn(config.topn)
+              .saveTopnAsHiveTable(spark, config.out, config.itemCol, config.itemListCol, config.overwrite,
+                config.partitionCol, config.partitionVal)
+          }
+        }
+        case "text" => {
+          if (config.saveScore.length>0) {
+            SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity()
+              .saveItemSimilarityAsTextFile(spark, config.saveScore, config.overwrite, config.fieldDelimiter, config.collectionDelimiter)
+              .computeTopn(config.topn)
+              .saveTopnAsTextFile(spark, config.out, config.overwrite, config.fieldDelimiter, config.collectionDelimiter)
+          }
+          else {
+            SwingModel.buildGraph(df, config.maxUser).computeItemSimilarity()
+              .computeTopn(config.topn)
+              .saveTopnAsTextFile(spark, config.out, config.overwrite, config.fieldDelimiter, config.collectionDelimiter)
+          }
         }
       }
     }
--- fxspark/fxspark/src/main/scala/fxspark/recommendation/SwingModel.scala
+++ fxspark/fxspark/src/main/scala/fxspark/recommendation/SwingModel.scala
@@ -1,33 +1,154 @@
 package fxspark.recommendation
 
-import org.apache.spark.sql.DataFrame
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions.lit
+import util.Random.shuffle
 
 object SwingModel {
-  def setup() = {
+  var edges: RDD[(String, Long)] = null
+  var itemSimilarity: RDD[(Long, Long, Double)] = null
+  var topnResult: RDD[(Long, Array[Long])] = null
 
+  def buildGraph(df: DataFrame, maxUser: Int): this.type = {
+    val userListByItem = df.rdd.map(row => (row.getLong(1), row.get(0).toString)).groupByKey().map(x => (x._1, x._2.toArray))
+    edges = userListByItem.map { case (item, userArray) =>
+      if (userArray.length > maxUser) {
+        (item, shuffle(userArray.toList).take(maxUser).toArray)
+      }else {
+        (item, userArray)
+      }
+    }.flatMap { case (item, userArray) =>
+      userArray.map(user => (user, item))
+    }.cache()
+    edges.first()
+    this
   }
 
-  def computeItemSimilarity(df: DataFrame): this.type = {
+  def computeItemSimilarity(): this.type = {
+    val userPairByItemPair = edges.groupByKey().filter(_._2.size >= 2).flatMap { case (user, itemIter) =>
+      val itemArray = itemIter.toArray.sorted
+      val itemPairByUser = new ArrayBuffer[(String, String)]()
+      for (i <- 0 until itemArray.length-1) {
+        for (j <- i+1 until itemArray.length) {
+          itemPairByUser.append((itemArray(i)+"_"+itemArray(j), user))
+        }
+      }
+      itemPairByUser
+    }.groupByKey().filter(_._2.size >= 2).flatMap { case (itemPair, userIter) =>
+      val userArray = userIter.toArray.sorted
+      val result = new ArrayBuffer[(String, String)]()
+      for (i <- 0 until userArray.length-1) {
+        for (j <- i+1 until userArray.length) {
+          result.append((itemPair, userArray(i)+","+userArray(j)))
+        }
+      }
+      result
+    }
+    val itemCountByUserPair = edges.map(x => (x._2, x._1)).groupByKey().filter(_._2.size >=2).flatMap { case (item, userIter) =>
+      val userArray = userIter.toArray.sorted
+      val userPair = new ArrayBuffer[(String, Long)]()
+      for (i <- 0 until userArray.length-1) {
+        for (j <- i+1 until userArray.length) {
+          userPair.append((userArray(i)+","+userArray(j), 1))
+        }
+      }
+      userPair
+    }.reduceByKey(_+_)
+    val itemPairSimilarity = userPairByItemPair.map {case (itemPair, userPair) =>
+      (userPair, itemPair)
+    }.join(itemCountByUserPair).map{ case (userPair, (itemPair, itemCount)) =>
+      (itemPair, 1.0/(1+itemCount))
+    }.reduceByKey(_+_).map { case (itemPair, score) =>
+      val splitedItemPair = itemPair.split("_")
+      val itemI = splitedItemPair(0).toLong
+      val itemJ = splitedItemPair(1).toLong
+      (itemI, itemJ, score)
+    }.cache()
+    itemPairSimilarity.first()
+    edges.unpersist(blocking = false)
+    itemSimilarity = itemPairSimilarity.union(itemPairSimilarity.map(x => (x._2, x._1, x._3))).cache()
+    itemSimilarity.first()
     this
   }
 
   def computeTopn(n: Int): this.type = {
-
+    topnResult = itemSimilarity.map(x => (x._1, (x._2, x._3))).groupByKey().map(x => (x._1, x._2.toArray)).map {case (item, itemScore) =>
+      val topnItem = itemScore.sortWith{ case (left, right) => left._2 > right._2 }.slice(0, n).map(_._1)
+      (item, topnItem)
+    }.cache()
+    topnResult.first()
+    itemSimilarity.unpersist(blocking = false)
+    this
   }
 
-  def saveTopnAsTextFile(): this.type = {
-
+  def saveTopnAsTextFile(spark:SparkSession, path: String, overwrite: Boolean = false,
+                         fieldDelimiter: String = "\t", collectionDelimiter: String = ","): this.type = {
+    import spark.implicits._
+    val df = topnResult.map(x => String.join(fieldDelimiter, x._1.toString, String.join(collectionDelimiter, x._2.map(_.toString):_*))).toDF()
+    var writer = df.write
+    writer = overwrite match {
+      case true => writer.mode("overwrite")
+      case false => writer.mode("append")
+    }
+    writer.text(path)
+    this
   }
 
-  def saveTopnAsHiveTable(): this.type = {
-
+  def saveTopnAsHiveTable(spark: SparkSession, tblName: String,
+                          itemCol: String = "item", itemListCol: String = "item_list",
+                          overwrite: Boolean = false,
+                          partitionCol: String = "", partitionVal: String = ""): this.type = {
+    import spark.implicits._
+    var df = topnResult.toDF(itemCol, itemListCol)
+    if (partitionCol.length > 0) {
+      df = df.withColumn(partitionCol, lit(partitionVal))
+    }
+    var writer = df.write
+    writer = overwrite match {
+      case true => writer.mode("overwrite")
+      case false => writer.mode("append")
+    }
+    if (partitionCol.length > 0) {
+      writer = writer.partitionBy(partitionCol)
+    }
+    writer.saveAsTable(tblName)
+    this
   }
 
-  def saveItemSimilarityASTextFile(): this.type = {
-
+  def saveItemSimilarityAsTextFile(spark: SparkSession, path: String,
+                                   overwrite: Boolean = false,
+                                   fieldDelimiter: String = "\t", collectionDelimiter: String = ","): this.type = {
+    import spark.implicits._
+    val df = itemSimilarity.map(x => String.join(fieldDelimiter, x._1.toString, x._2.toString, x._3.toString)).toDF()
+    var writer = df.write
+    writer = overwrite match {
+      case true => writer.mode("overwrite")
+      case false => writer.mode("append")
+    }
+    writer.text(path)
+    this
   }
 
-  def saveItemSimilarityAsHiveTable(): this.type = {
-
+  def saveItemSimilarityAsHiveTable(spark: SparkSession, tblName: String,
+                                    itemICol: String = "item_i", itemJCol: String = "item_j", scoreCol: String = "score",
+                                    overwrite: Boolean = false,
+                                    partitionCol: String = "", partitionVal: String = ""): this.type = {
+    import spark.implicits._
+    var df = itemSimilarity.toDF(itemICol, itemJCol, scoreCol)
+    if (partitionCol.length > 0) {
+      df = df.withColumn(partitionCol, lit(partitionVal))
+    }
+    var writer = df.write
+    writer = overwrite match {
+      case true => writer.mode("overwrite")
+      case false => writer.mode("append")
+    }
+    if (partitionCol.length > 0) {
+      writer = writer.partitionBy(partitionCol)
+    }
+    writer.saveAsTable(tblName)
+    this
   }
 }
